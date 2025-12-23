19aed3f14668844da672c93300944ef7057d0326
WilliamCWhite <willamcwhite323@gmail.com>
2025-12-22 22:54:14 -0500

    Battled with Freezed for a while, determined that it should not be used

--- lib/shared/models/book_model.dart
+++ lib/shared/models/book_model.dart
@@ -10,4 +10,15 @@ class BookModel {
     required this.color,
     required this.content,
   });
+
+  // Optional: A manual "copyWith" method in case you need it later.
+  // You don't need this to run the app right now, but it's useful to have.
+  BookModel copyWith({int? id, String? title, String? color, String? content}) {
+    return BookModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      color: color ?? this.color,
+      content: content ?? this.content,
+    );
+  }
 }
